7324bb473279d335c375b36bf8967d4fdbaa89fb
Dluski Kirill <nata.sobacka@yandex.by>
2023-04-01 14:51:51 +0300

    Добавьте файлы проекта.

---
+++ lab2_2/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace lab2_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(190, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "18 16 28 12 14";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Значения узлов";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(221, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 20);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Применить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(25, 90);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 44);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Выпонить прямой обход";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(127, 103);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(169, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(397, 18);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(391, 367);
+            this.panel1.TabIndex = 14;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(25, 327);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "Нарисовать";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Visible = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(25, 164);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 23);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Балансировка";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private static System.Windows.Forms.TextBox outbox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ lab2_2/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+
+
+namespace lab2_2
+{
+    public partial class Form1 : Form
+    {
+        public int COUNT = 10;
+
+        public class Node
+        {
+            public int Key;
+            public int height;
+            public Node left;
+            public Node right;
+            public int R = 15;
+
+            public Node(int key)
+            {
+                Key= key;
+                left = null; 
+                right = null;
+                height= 1;
+            }
+            internal void Risovat(Graphics g, int x, int y, int dx, int dy)
+            {
+                if (left != null)
+                    g.DrawLine(Pens.Black, x, y, x - dx / 2, y + dy);
+                if (right != null)
+                    g.DrawLine(Pens.Black, x, y, x + dx / 2, y + dy);
+
+                g.FillEllipse(Brushes.White, x - R, y - R, 2 * R, 2 * R);
+                g.DrawEllipse(Pens.Black, x - R, y - R, 2 * R, 2 * R);
+                g.DrawString(Key.ToString(), new Font("Calibri", 10), Brushes.Black, x - 8, y - 8);
+                g.DrawString(height.ToString(), new Font("Calibri", 10), Brushes.Red, x - 20, y - 30);
+
+                if (left != null)
+                    left.Risovat(g, x - dx / 2, y + dy, dx / 2, dy);
+                if (right != null)
+                    right.Risovat(g, x + dx / 2, y + dy, dx / 2, dy);
+            }
+        }
+
+        class Tree
+        {
+            public string txtnodes;
+            public int comp = 1;
+            public Node root;
+
+            public void PreOrder(Node localroot)
+            {
+                if (localroot != null)
+                {
+                    txtnodes += (Convert.ToString(localroot.Key) + " ");
+                    PreOrder(localroot.left);
+                    PreOrder(localroot.right);
+                }
+            }
+
+
+            public Node Insert(Node node, int key)
+            {
+                // Если дерево пустое, создаем новый узел с ключом
+                if (node == null)
+                    return new Node(key);
+
+                // Если ключ меньше ключа узла, вставляем его в левое поддерево
+                if (key < node.Key)
+                    node.left = Insert(node.left, key);
+
+                // Если ключ больше ключа узла, вставляем его в правое поддерево
+                else if (key > node.Key)
+                    node.right = Insert(node.right, key);
+
+                // Если ключ равен ключу узла, ничего не делаем (предполагаем, что дерево не содержит дубликатов)
+                else
+                    return node;
+
+                // Обновляем высоту узла после вставки
+                UpdateHeight(node);
+                return node;
+            }
+
+            public void Delete(Node localroot)
+            {
+                if (localroot != null)
+                {
+                    txtnodes += (Convert.ToString(localroot.Key) + " ");
+                    if (localroot.left != null)
+                    {
+                        if (localroot.left.left == null && localroot.left.right == null)
+                        {
+                            localroot.left = null;
+                        }
+                        else { Delete(localroot.left); }
+                    }
+                    if (localroot.right != null)
+                    {
+                        if (localroot.right.left == null && localroot.right.right == null)
+                        {
+                            localroot.right = null;
+                        }
+                        else { Delete(localroot.right); }
+                    }
+                }
+            }
+
+            public int GetHeight(Node node)
+            {
+                if (node == null)
+                    return 0;
+                return node.height;
+            }
+
+            public int GetBalance(Node node)
+            {
+                if (node == null)
+                    return 0;
+                return GetHeight(node.left) - GetHeight(node.right);
+            }
+
+            public void UpdateHeight(Node node)
+            {
+                if (node == null)
+                    return;
+                node.height = Math.Max(GetHeight(node.left), GetHeight(node.right)) + 1;
+            }
+
+            public Node RightRotate(Node y)
+            {
+                Node x = y.left;
+                Node z = x.right;
+
+                // Поворот
+                x.right = y;
+                y.left = z;
+
+                // Обновление высот
+                UpdateHeight(y);
+                UpdateHeight(x);
+
+                // Возвращаем новый корень поддерева
+                return x;
+            }
+
+            public Node LeftRotate(Node x)
+            {
+                Node y = x.right;
+                Node z = y.left;
+
+                // Поворот
+                y.left = x;
+                x.right = z;
+
+                // Обновление высот
+                UpdateHeight(x);
+                UpdateHeight(y);
+
+                // Возвращаем новый корень поддерева
+                return y;
+            }
+
+            public void Balance(Node node)
+            {
+                int balance = GetBalance(node);
+
+                // Если узел несбалансирован, то есть 4 случая
+
+                // Левый левый случай
+                if (balance > 1 && node.Key < node.left.Key)
+                    RightRotate(node);
+
+                // Правый правый случай
+                if (balance < -1 && node.Key > node.right.Key)
+                    LeftRotate(node);
+
+                // Левый правый случай
+                if (balance > 1 && node.Key > node.left.Key)
+                {
+                    node.left = LeftRotate(node.left);
+                    RightRotate(node);
+                }
+
+                // Правый левый случай
+                if (balance < -1 && node.Key < node.right.Key)
+                {
+                    node.right = RightRotate(node.right);
+                    LeftRotate(node);
+                }
+            }
+
+            public bool Find(int key) // Поиск узла с заданным ключом
+            { // (предполагается, что дерево не пустое)
+                Node current = root; // Начать с корневого узла
+                while (current.Key != key) // Пока не найдено совпадение
+                {
+                    if (key < current.Key) // Двигаться налево?
+                        current = current.left;
+                    else
+                        current = current.right; // Или направо?
+                    comp++;
+                    if (current == null) // Если потомка нет,
+                        return false; // поиск завершился неудачей
+                }
+                return true; // Элемент найден
+            }
+
+
+            public int Maxdepth(Node root)
+            {
+                if (root == null) return 0;
+                else return Math.Max(Maxdepth(root.left), Maxdepth(root.right)) + 1;
+            }
+
+
+        }
+
+
+
+        //public void getout(Node root, int space)
+        //{
+        //    if (root == null)
+        //        return;
+
+        //    // Increase distance between levels
+        //    space += COUNT;
+
+        //    // Process right child first
+        //    getout(root.right, space);
+
+        //    // Print current node after space
+        //    // count
+        //    textBox5.Text += "\r\n";
+        //    for (int i = COUNT; i < space; i++)
+        //        textBox5.Text += " ";
+        //    textBox5.Text += Convert.ToString(root.Key) + "\r\n";
+
+
+        //    // Process left child
+        //    getout(root.left, space);
+        //}
+
+        Tree theTree = new Tree();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            theTree.root = null;
+            int[] arr = Array.ConvertAll(textBox1.Text.Split(), s => int.Parse(s));
+            foreach (var VARIABLE in arr)
+            {
+                theTree.root=theTree.Insert(theTree.root, VARIABLE);
+            }
+
+            //Graphics g = panel1.CreateGraphics();
+            //theTree.root.Risovat(g, panel1.Width / 2, 50, panel1.Height / 2, 50);
+            button6_Click(sender, e);
+            //textBox5.Text = "";
+            //getout(theTree.root, 0);
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            theTree.txtnodes = "";
+            textBox2.Text = "";
+            theTree.PreOrder(theTree.root);
+            textBox2.Text = theTree.txtnodes;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            theTree.Delete(theTree.root);
+
+            button6_Click(sender, e);
+            //textBox5.Text = "";
+            //getout(theTree.root, 0);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            g.Clear(Color.White);
+            theTree.root.Risovat(g, panel1.Width / 2, 50, panel1.Height / 2, 50);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            theTree.Balance(theTree.root);
+            button6_Click(sender, e);
+        }
+    }
+}
